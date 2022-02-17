02a03b3c65a776cbfc227b2c4fbf9a0ceb69484f
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2022-02-17 02:54:25 -0300

    ✨ add favicon and page title

--- app.vue
+++ app.vue
@@ -1,14 +1,36 @@
 <template>
   <Link rel="preconnect" href="https://fonts.googleapis.com" />
+
   <Link
     rel="preconnect"
     href="https://fonts.gstatic.com"
     crossorigin="crossorigin"
   />
+
   <Link
     rel="stylesheet"
     href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@600;700&family=Zilla+Slab:wght@600&display=swap"
   />
+
+  <Link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+
+  <Link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+
+  <Link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+
+  <Link rel="manifest" href="/site.webmanifest" />
+
+  <Link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+
+  <Meta name="apple-mobile-web-app-title" content="Cometa" />
+
+  <Meta name="application-name" content="Cometa" />
+
+  <Meta name="msapplication-TileColor" content="#da532c" />
+
+  <Meta name="theme-color" content="#ffffff" />
+
+  <Title>Cometa</Title>
 
   <main>
     <The1stSection />
