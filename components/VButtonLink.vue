dc3758b442953c83281e050325808dcfc57359ce
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2022-02-25 21:57:01 -0300

    ✨ open Typeform in the CTA

--- components/VButtonLink.vue
+++ components/VButtonLink.vue
@@ -1,12 +1,36 @@
 <template>
-  <a class="VButtonLink" href="#">
+  <a
+    class="VButtonLink"
+    rel="noopener noreferrer"
+    href="https://bianca536991.typeform.com/to/P5IzkG6J"
+    target="_blank"
+    @click.prevent="handleClick"
+  >
     <span class="VButtonLink__text">
       <slot></slot>
     </span>
   </a>
 </template>
 
+<script lang="ts" setup>
+import type { Popup } from '@typeform/embed';
+
+let popup: Popup | null = null;
+
+onMounted(async () => {
+  const { createPopup } = await import('@typeform/embed');
+
+  popup = createPopup('P5IzkG6J');
+});
+
+const handleClick = () => {
+  popup?.open();
+};
+</script>
+
 <style lang="scss">
+@import '@typeform/embed/build/css/popup.css';
+
 .VButtonLink {
   display: inline-flex;
   align-items: center;
